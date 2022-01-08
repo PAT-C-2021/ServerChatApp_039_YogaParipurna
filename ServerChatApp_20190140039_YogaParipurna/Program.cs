20f88808a6919a03be248363ba6698656e175377
Yoga Paripurna <63484583+YogaParipurna@users.noreply.github.com>
2022-01-08 09:53:41 +0700

    Add project files.

---
+++ ServerChatApp_20190140039_YogaParipurna/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using WCFService_2Way_20190140039;
+
+namespace ServerChatApp_20190140039_YogaParipurna
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost hostObj = null;
+            try
+            {
+                hostObj = new ServiceHost(typeof(ServiceCallback));
+                hostObj.Open();
+                Console.WriteLine("Server dah siap!!!!");
+                Console.ReadLine();
+                hostObj.Close();
+            }
+            catch (Exception ex)
+            {
+                hostObj = null;
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
+        }
+
+    }
+}
